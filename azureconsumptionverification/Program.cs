bd0d049cabc96dd74e72e2709b39333138203642
michaelbaranov <horseman.ua@gmail.com>
2020-05-10 19:53:39 +0300

    Update help

--- azureconsumptionverification/Program.cs
+++ azureconsumptionverification/Program.cs
@@ -164,18 +164,35 @@ namespace AzureConsumptionVerification
             Console.WriteLine(" -clientId");
             Console.WriteLine(" -clientSecret");
             Console.WriteLine(" -tenantId");
-            Console.WriteLine(" -subscriptionId");
+            Console.WriteLine(" -subscription [mandatory] supported values - <all>, <array of subscription ids>");
             Console.WriteLine(
                 " -numberOfMonths [optional, default 1] number of months to analyze, due to Activity log API limitation in 90 days max value is 4");
             Console.WriteLine(
                 " -outputFolder [optional, default %TEMP%] folder to save report");
             Console.WriteLine(
                 " -openReport [optional, default <empty>] switch if enabled opens generated report");
-            Console.WriteLine("Example:");
+            Console.WriteLine("Examples:");
+            Console.WriteLine("Run analysis for all subscriptions available for service principal");
             Console.WriteLine("AzureConsumptionVerification -cilentId=124d8317-dd0a-47f8-b630-c4839eb1602d " +
                               "-clientSecret=ObTY9A53gEB3_TgUFICK=gqX_NedhlE- " +
                               "-tenantId=91700184-c314-4dc9-bb7e-a411df456a1e " +
-                              "-subscriptionId=38cadfad-6513-4396-af97-8606962edfa1" +
+                              "-subscription=all" +
+                              "-outputFolder=c:\\reports" +
+                              "-openReport ");
+            Console.WriteLine("------------------------------------------------------------------");
+            Console.WriteLine("Run analysis for single subscription");
+            Console.WriteLine("AzureConsumptionVerification -cilentId=124d8317-dd0a-47f8-b630-c4839eb1602d " +
+                              "-clientSecret=ObTY9A53gEB3_TgUFICK=gqX_NedhlE- " +
+                              "-tenantId=91700184-c314-4dc9-bb7e-a411df456a1e " +
+                              "-subscription=22d1e318-4c86-4f8e-9cef-b04f36ba31c0" +
+                              "-outputFolder=c:\\reports" +
+                              "-openReport ");
+            Console.WriteLine("------------------------------------------------------------------");
+            Console.WriteLine("Run analysis for two subscriptions");
+            Console.WriteLine("AzureConsumptionVerification -cilentId=124d8317-dd0a-47f8-b630-c4839eb1602d " +
+                              "-clientSecret=ObTY9A53gEB3_TgUFICK=gqX_NedhlE- " +
+                              "-tenantId=91700184-c314-4dc9-bb7e-a411df456a1e " +
+                              "-subscription=22d1e318-4c86-4f8e-9cef-b04f36ba31c0,1e31822d-5c86-4a8e-9cef-f36ba31c0b04" +
                               "-outputFolder=c:\\reports" +
                               "-openReport ");
         }
