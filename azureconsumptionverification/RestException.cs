93f05314fb5882ad8fe1ec5cc3211c4c33ec305c
michaelbaranov <horseman.ua@gmail.com>
2020-05-18 16:28:03 +0300

    Files were missing from prev commit

---
+++ azureconsumptionverification/RestException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+
+namespace AzureConsumptionVerification
+{
+    internal class RestException : Exception
+    {
+        public string Code;
+        public string ResponseMessage;
+        public HttpStatusCode StatusCode;
+
+        public RestException(string? message, HttpStatusCode statusCode, string code, string responseMessage) :
+            base(message)
+        {
+            StatusCode = statusCode;
+            Code = code;
+            ResponseMessage = responseMessage;
+        }
+    }
+}
---
+++ azureconsumptionverification/RestService.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace AzureConsumptionVerification
+{
+    internal class RestService
+    {
+        private static readonly HttpClient _httpClient = new HttpClient();
+
+        public static async Task<string> SendAsync(HttpRequestMessage message)
+        {
+            var response = await _httpClient.SendAsync(message);
+            if (!response.IsSuccessStatusCode)
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+
+                var jObject = JObject.Parse(responseContent);
+                throw new RestException("Not successful status code", response.StatusCode,
+                    jObject["code"].Value<string>(), jObject["message"].Value<string>());
+            }
+
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
